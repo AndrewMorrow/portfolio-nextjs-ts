ac0b863a6f37a1a439f5306fa436f2423f81504f
Andrew Morrow <beardedmongo@gmail.com>
2022-02-01 13:20:06 -0600

    add Header

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -66,9 +66,6 @@ export default function Example() {
                   </a>
                 </div>
               </div>
-              <div className="absolute inset-y-0 right-0 flex items-center pr-2 sm:static sm:inset-auto sm:ml-6 sm:pr-0 text-lg md:text-xl font-bold">
-                Andrew Morrow
-              </div>
             </div>
           </div>
 
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,6 @@
 import type { NextPage } from "next";
 import Head from "next/head";
+import Header from "../components/Header";
 import Navbar from "../components/Navbar";
 import Navbar2 from "../components/Navbar";
 
@@ -12,48 +13,7 @@ const Home: NextPage = () => {
         <link rel="icon" href="./favicon.ico" />
       </Head>
       <Navbar2 />
-     
-      <div className="">
-        <p className="">
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Quod natus
-          facilis quasi officiis dignissimos enim, fuga saepe officia dolorem
-          sequi quaerat unde eius obcaecati totam. Recusandae vitae dicta
-          pariatur quos. Lorem ipsum dolor sit amet consectetur adipisicing
-          elit. Iusto accusantium expedita saepe, adipisci et officia ullam
-          nulla eos necessitatibus velit similique labore, nihil iure rem
-          repellat reprehenderit. Corporis praesentium sed minima quos
-          laboriosam sequi magnam dolorem, distinctio deserunt asperiores vel
-          odio necessitatibus incidunt aliquam illum voluptas non, aspernatur
-          aliquid atque? Et id expedita dicta consequatur amet nisi esse.
-          Molestias dignissimos sapiente maxime. Dicta inventore facere, itaque
-          fugiat id sapiente labore in laboriosam animi blanditiis autem impedit
-          at molestiae veniam nam eaque dolorum expedita! Unde facilis,
-          consequuntur aliquam veritatis ratione quis sint eveniet ea sapiente
-          at, libero corrupti magni iure obcaecati nesciunt dolores tempora.
-          Odio laudantium harum perferendis eveniet sint eligendi dignissimos
-          ducimus nobis alias id voluptas natus, quidem amet voluptatem ipsum
-          optio quam, velit reprehenderit, aut dolor. Pariatur ad neque dolorem,
-          dolor reiciendis harum consequuntur optio facilis corrupti tempora
-          quasi expedita, assumenda quisquam accusantium at molestias minima a
-          voluptatem! Delectus beatae, nemo asperiores distinctio explicabo et
-          officia ullam, sequi fuga facilis nam quis minima unde recusandae a
-          velit obcaecati atque laboriosam facere ratione repellat fugit! Fugiat
-          similique quis blanditiis veritatis ipsa amet beatae nemo rerum
-          tempore natus velit ex laboriosam impedit, deleniti quibusdam ad
-          corporis at obcaecati veniam minus quasi officia eum provident
-          cupiditate. Tempore, odit quas eius architecto error suscipit aut
-          aspernatur, exercitationem perspiciatis, recusandae maiores eligendi
-          neque voluptatum dolores similique placeat assumenda ex minus. Quidem
-          recusandae debitis ad sapiente deserunt illum omnis maxime laborum!
-          Omnis voluptate ea animi, explicabo laudantium placeat dolor
-          asperiores fuga libero doloribus sunt? Ipsum quaerat iusto delectus
-          ducimus omnis! Nisi laudantium explicabo, nobis dolorem, velit enim,
-          libero ducimus ullam illum labore in cum fugit saepe! Libero, omnis.
-          Nemo omnis ex quaerat, natus cupiditate suscipit quia sapiente ipsam
-          illum dicta facilis quis nostrum enim eveniet commodi eaque non
-          inventore? Expedita at molestias nam facere tempora!
-        </p>
-      </div>
+      <Header />
     </div>
   );
 };
