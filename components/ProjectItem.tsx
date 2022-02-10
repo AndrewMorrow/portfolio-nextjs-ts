4c41c811b005f6a9d132bc0324a1b513e7835bf5
Andrew Morrow <beardedmongo@gmail.com>
2022-02-10 16:45:57 -0600

    updates project Images positioning

--- components/ProjectItem.tsx
+++ components/ProjectItem.tsx
@@ -6,26 +6,35 @@ import laptopImage from "../public/images/LaptopSmerrimentquest.png";
 export default function ProjectItem() {
   return (
     <div className="w-screen flex justify-center">
-      <div className="flex relative p-10 w-[32rem] h-min">
-        <Image className="  " src={laptopImage} alt="merriment" />
-        <span className="absolute top-44 right-0">
-          <Image
-            className="object-contain "
-            src="/images/iPhone12merrimentquest.png"
-            alt="Image alt here"
-            height={150}
-            width={250}
-          />
-        </span>
-        <span className="absolute top-32 ">
-          <Image
-            className="object-contain"
-            src="/images/iPadPro11_merrimentquest.png"
-            alt="Image alt here"
-            height={200}
-            width={200}
-          />
-        </span>
+      <div className="flex flex-wrap relative w-full sm:w-3/4 md:w-1/2 h-96 px-4">
+        {/* <Image
+          className="  "
+          src={laptopImage}
+          alt="merriment"
+          width={600}
+          height={400}
+          
+        /> */}
+        <div className="bg-[url('/images/LaptopSmerrimentquest.png')] bg-no-repeat  bg-contain w-min min-w-full">
+          <div className="absolute bottom-10 right-2 lg:top-32 lg:-right-6">
+            <Image
+              className="object-contain "
+              src="/images/iPhone12merrimentquest.png"
+              alt="Image alt here"
+              height={200}
+              width={100}
+            />
+          </div>
+          <div className="absolute bottom-10 left-2 md:bottom-15 md:-left-10 lg:top-20 lg:-left-24">
+            <Image
+              className="object-contain"
+              src="/images/iPadPro11_merrimentquest.png"
+              alt="Image alt here"
+              height={250}
+              width={180}
+            />
+          </div>
+        </div>
       </div>
     </div>
   );
