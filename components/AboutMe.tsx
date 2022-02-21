bc78f35b9cae7d591296f99211a605ee9f08f593
Andrew Morrow <beardedmongo@gmail.com>
2022-02-20 22:07:19 -0600

    fixes tooltip positioning

--- components/AboutMe.tsx
+++ components/AboutMe.tsx
@@ -13,109 +13,154 @@ export default function AboutMe() {
       <div className="flex  py-20 md:py-24">
         <div className="w-1/2">
           <h2 className="font-bold text-xl mb-2">About Me</h2>
-          <p className="">
+          <p className="mb-5">
             I currently work with JavaScript, jQuery, Bootstrap and React on a
             daily basis. I enjoy expanding my knowledge through learning and
             teaching different techologies. Outside of coding I also have a
             passion for cooking and gardening.
           </p>
+          <div className="mb-5">
+            <h3 className="font-semibold mb-1">
+              Section 508 Trusted Tester Conformance
+            </h3>
+            <p>
+              I am a 508 accessibility certified through the Department of
+              Homeland Security.{" "}
+            </p>
+          </div>
+          <div>
+            <h3 className="font-semibold mb-1">
+              Public Trust Security Clearance
+            </h3>
+            <p>
+              I have public trust security clearance through the Department of
+              Homeland Security.
+            </p>
+          </div>
         </div>
         <div className="flex flex-col w-1/2 pl-10 pr-3">
           <h3 className="text-xl font-bold mb-3 ">Technology Proficiencies</h3>
           <ul className="flex flex-col ">
-            <li className="flex has-tooltip relative mb-3">
-              <a href="www.google.com" className="flex cursor-pointer ">
+            <li className="flex  mb-3">
+              <a
+                href="www.google.com"
+                className="flex cursor-pointer has-tooltip relative"
+              >
                 <span className="self-center font-semibold mr-2">HTML</span>{" "}
                 <FaHtml5 size={30} />
-                <span className="tooltip p-1 rounded-md right-1/4">
-                  Learn More
+                <span className="tooltip p-1 rounded-md left-1/2 translate-x-12 min-w-max">
+                  Click to learn more
                 </span>
               </a>
             </li>
-            <li className="flex has-tooltip relative mb-3">
-              <a href="www.google.com" className="flex cursor-pointer ">
+            <li className="flex  mb-3">
+              <a
+                href="www.google.com"
+                className="flex cursor-pointer has-tooltip relative"
+              >
                 <span className="self-center font-semibold mr-2"> CSS </span>
                 <FaCss3Alt size={30} />
-                <span className="tooltip p-1 rounded-md right-1/3">
-                  Learn More
+                <span className="tooltip p-1 rounded-md left-1/2 translate-x-10 min-w-max">
+                  Click to learn more
                 </span>
               </a>
             </li>
-            <li className="flex has-tooltip relative mb-3">
-              <a href="www.google.com" className="flex cursor-pointer ">
+            <li className="flex  mb-3">
+              <a
+                href="www.google.com"
+                className="flex cursor-pointer has-tooltip relative"
+              >
                 <span className="self-center font-semibold mr-2">
                   {" "}
                   JavaScript{" "}
                 </span>
                 <SiJavascript size={30} />
-                <span className="tooltip p-1 rounded-md right-6">
-                  Learn More
+                <span className="tooltip p-1 rounded-md left-1/2 translate-x-16 min-w-max">
+                  Click to learn more
                 </span>
               </a>
             </li>
-            <li className="flex has-tooltip relative mb-3">
-              <a href="www.google.com" className="flex cursor-pointer ">
+            <li className="flex  mb-3">
+              <a
+                href="www.google.com"
+                className="flex cursor-pointer has-tooltip relative"
+              >
                 <span className="self-center font-semibold mr-2"> NodeJS </span>
                 <FaNodeJs size={30} />
-                <span className="tooltip p-1 rounded-md right-11">
-                  Learn More
+                <span className="tooltip p-1 rounded-md left-1/2 translate-x-14 min-w-max">
+                  Click to learn more
                 </span>
               </a>
             </li>
-            <li className="flex has-tooltip relative mb-3">
-              <a href="www.google.com" className="flex cursor-pointer ">
+            <li className="flex  mb-3">
+              <a
+                href="www.google.com"
+                className="flex cursor-pointer has-tooltip relative"
+              >
                 <span className="self-center font-semibold mr-2">
                   {" "}
                   ReactJS{" "}
                 </span>
                 <FaReact size={30} />
-                <span className="tooltip p-1 rounded-md right-11">
-                  Learn More
+                <span className="tooltip p-1 rounded-md left-1/2 translate-x-14 min-w-max">
+                  Click to learn more
                 </span>
               </a>
             </li>
-            <li className="flex has-tooltip relative mb-3">
-              <a href="www.google.com" className="flex cursor-pointer ">
+            <li className="flex mb-3">
+              <a
+                href="www.google.com"
+                className="flex cursor-pointer has-tooltip relative"
+              >
                 <span className="self-center font-semibold mr-2"> MySQL </span>
                 <SiMysql size={30} />
-                <span className="tooltip p-1 rounded-md right-11">
-                  Learn More
+                <span className="tooltip p-1 rounded-md left-1/2 translate-x-14 min-w-max">
+                  Click to learn more
                 </span>
               </a>
             </li>
-            <li className="flex has-tooltip relative mb-3">
-              <a href="www.google.com" className="flex cursor-pointer ">
+            <li className="flex mb-3">
+              <a
+                href="www.google.com"
+                className="flex cursor-pointer has-tooltip relative"
+              >
                 <span className="self-center font-semibold mr-2">
                   {" "}
                   MongoDB{" "}
                 </span>
                 <SiMongodb size={30} />
-                <span className="tooltip p-1 rounded-md right-8">
-                  Learn More
+                <span className="tooltip p-1 rounded-md left-1/2 translate-x-14 min-w-max">
+                  Click to learn more
                 </span>
               </a>
             </li>
-            <li className="flex has-tooltip relative mb-3">
-              <a href="www.google.com" className="flex cursor-pointer ">
+            <li className="flex  mb-3">
+              <a
+                href="www.google.com"
+                className="flex cursor-pointer has-tooltip relative"
+              >
                 <span className="self-center font-semibold mr-2">
                   {" "}
                   TypeScript{" "}
                 </span>
                 <SiTypescript size={30} />
-                <span className="tooltip p-1 rounded-md right-5">
-                  Learn More
+                <span className="tooltip p-1 rounded-md left-1/2 translate-x-16 min-w-max">
+                  Click to learn more
                 </span>
               </a>
             </li>
-            <li className="flex has-tooltip relative mb-3">
-              <a href="www.google.com" className="flex cursor-pointer ">
+            <li className="flex  mb-3">
+              <a
+                href="www.google.com"
+                className="flex cursor-pointer has-tooltip relative"
+              >
                 <span className="self-center font-semibold mr-2">
                   {" "}
                   GraphQL{" "}
                 </span>
                 <SiGraphql size={30} />
-                <span className="tooltip p-1 rounded-md right-8">
-                  Learn More
+                <span className="tooltip p-1 rounded-md left-1/2 translate-x-14 min-w-max">
+                  Click to learn more
                 </span>
               </a>
             </li>
